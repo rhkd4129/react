c66b30bfc2b017aa9bb6a01e797ef833f9c7550c
Lee <rhkd4129@naver.com>
2023-05-02 18:56:33 +0900

    a

---
+++ dreamcoding/1.js
@@ -0,0 +1,18 @@
+
+'use strict';
+
+
+
+
+{
+
+    let name = 'ellie';
+    console.log(name);
+    name = 'hello';
+    console.log(name)
+    
+}
+
+
+// 호이스팅 어디에 선언했냐 상관없이 젤 먼저 선언되는 것  , block scope 해당 x
+
