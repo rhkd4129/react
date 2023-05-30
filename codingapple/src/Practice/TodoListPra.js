39b864583e3921a795aeda293458513246816585
Lee <rhkd4129@naver.com>
2023-05-30 20:30:44 +0900

    todoList

--- codingapple/src/Practice/TodoListPra.js
+++ codingapple/src/Practice/TodoListPra.js
@@ -1,29 +1,89 @@
 import React from "react";
-// import { useState } from 'react';
+import { useState } from 'react';
+
+// class TodoItem extends React.Component{
+
+//     render(){
+//         const {todo} = this.props
+//         return(
+//             <div><li>{todo}</li></div>
+//         );
+//     }
+// }
 
 
-class TodoListPra extends React.Component{
+// class TodoListPra extends React.Component{
+//     state={
+//         todoList:['파이썬','장고'],
+//         current:'',
+//     }
+//     onChange = (e)=>{
+//         const {value} = e.target
+//         this.setState({current:value})
+//     }
+//     onKeyDown=(e)=>{
+//         if(e.keyCode ==13){
+//             const {todoList, current} = this.state
+//             this.setState({
+//                 current:'',
+//                 todoList:[...todoList,current]
+//             })
+//         }
+//     }
+//     render(){
+//         return(
+//             <div>
+//                 <ul>
+//                     {this.state.todoList.map((todo,index) =><TodoItem  key ={index} todo={todo} /> )}
+                   
+//                 </ul>
 
-    state={
-        todoList:['파이썬','장고'],
-    }
-
-    render(){
-        return(
-            <div>
-                <ul>
-                    <li>{this.state.todoList[0]}</li>
-                    <li>{this.state.todoList[1]}</li>
-                </ul>
-
-                <input></input>
-            </div>
-        );
-    }
+//                 <input onChange={this.onChange}
+//                 onKeyDown={this.onKeyDown}
+//                 value = {this.state.current}
+//                 ></input>
+            
+//             </div>
+//         );
+//     }
       
 
 
-}
+// }
 
+const TodoItem = ({todo})=> <li>{todo}</li>
+
+function TodoListPra({todo}){
+    const [TodoItems,setTodoItem] = useState(
+        {todoList:['장고','파이썬'],
+         current:''
+    });
+
+
+    const onchange = (e)=>{
+        const value = e.target.value
+        setTodoItem((prevState)=>({...prevState,current:value}))
+    }
+    const onKeyDown = (e) => {
+        if (e.keyCode === 13) {
+          setTodoItem((prevState) => ({
+    
+            todoList: [...prevState.todoList, prevState.current],
+            current: ''
+          }));
+        }
+      };
+    return(
+        <div>
+            <ul>
+                {TodoItems.todoList.map((todo,index)=>(<TodoItem key={index} todo={todo}></TodoItem>))}
+            </ul>
+            <input onChange={onchange}
+            onKeyDown={onKeyDown}
+            value={TodoItems.current}></input>
+            <p>{TodoItems.current}</p>
+        </div>
+    );
+}
 
 export default TodoListPra;
